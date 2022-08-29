7f1bcc12bb6f6515d52b429d5ab9aadae8bc04e6
PGV1000 <ogloyhorhoy73@gmail.com>
2022-08-29 18:29:42 +0300

    Implemented new algorith using rasterio, fixed a problem with output size and clipping it to input extent

--- Merge.py
+++ Merge.py
@@ -1,10 +1,44 @@
-from types import CoroutineType
-from osgeo import gdal
-import numpy as np
+# Gdal Algorithm - has a problem with output size and clipping it to input extent
+# from types import CoroutineType
+# from osgeo import gdal
+# import numpy as np
+# import glob
+
+# # Grab a tif file name 
+# imageName = glob.glob('*.tif')[0]
+
+# # Open this tif with gdal and put it in "image" variable
+# image = gdal.Open(imageName)
+
+# # Clip an image with "Mask.shp" and return "Masked.tif"
+# maskedImage = gdal.Warp("Masked.tif", image, cutlineDSName = 'Mask.shp', cropToCutline = True, dstNodata = np.nan)
+# image = maskedImage = None
+
+import fiona
+import rasterio
+import rasterio.mask
 import glob
 
 
-imageName = glob.glob('*.tif')[0] # Grab a tif file name 
-image = gdal.Open(imageName) # Open this tif with gdal and put it in "image" variable
-maskedImage = gdal.Warp("Masked.tif", image, cutlineDSName = 'Mask.shp', cropToCutline = True, dstNodata = np.nan)
-image = maskedImage = None
+src_raster_path = glob.glob('*.tif')[0]
+
+shp_file_path = "Mask.shp"
+
+output_raster_path = src_raster_path[:-3] + '_masked.tif'
+
+with fiona.open(shp_file_path, "r") as shapefile:
+    shapes = [feature["geometry"] for feature in shapefile]
+
+with rasterio.open(src_raster_path) as src:
+
+    out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
+    out_meta = src.meta
+
+out_meta.update({"driver": "GTiff",
+                 "height": out_image.shape[1],
+                 "width": out_image.shape[2],
+                 "transform": out_transform})
+
+
+with rasterio.open(output_raster_path, "w", **out_meta) as dest:
+    dest.write(out_image)
