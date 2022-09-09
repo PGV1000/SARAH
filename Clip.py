1a5ed6296d60f6567abf47d80e2cb67791c4d7a9
PGV1000 <ogloyhorhoy73@gmail.com>
2022-09-09 14:34:24 +0300

    New Polygonize module

--- Clip.py
+++ Clip.py
@@ -25,18 +25,23 @@ print('Starting clipping...')
 
 print('Getting the image file...')
 src_raster_path = glob.glob('./Clip/*.tif')[0]
+
 print('Getting the mask file...')
 shp_file_path = glob.glob('./Clip/*.shp')[0]
+
 print('Creating the output file name...')
 output_raster_path = './Clip/Masked_' + os.path.basename(src_raster_path)
+
 print('Opening the mask file... ')
 with fiona.open(shp_file_path, "r") as shapefile:
     shapes = [feature["geometry"] for feature in shapefile]
+    
 print('Opening the image file...')
 with rasterio.open(src_raster_path) as src:
     print('Clipping the image...')
     out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
     out_meta = src.meta
+
 print('Configuring the output file metadata...')
 out_meta.update({"driver": "GTiff",
                  "height": out_image.shape[1],
@@ -46,4 +51,5 @@ out_meta.update({"driver": "GTiff",
 print('Saving the output file...')
 with rasterio.open(output_raster_path, "w", **out_meta) as dest:
     dest.write(out_image)
+    
 print('Done')
