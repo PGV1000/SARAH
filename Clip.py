66e20d4214064ee62a641beae9adbcdf41390868
PGV1000 <ogloyhorhoy73@gmail.com>
2022-09-13 20:02:50 +0300

    New output parameter, paths changes

--- Clip.py
+++ Clip.py
@@ -4,29 +4,28 @@ os.environ['GDAL_DATA'] = 'C:\OSGeo4W\apps\gdal\share\gdal'
 import fiona
 import rasterio
 import rasterio.mask
-import glob
 
-def Clip(maskPath):
+def Clip(input_raster_path,maskPath,outputRasterPath):
     print('Starting clipping...')
 
     print('Getting the image file...')
-    src_raster_path = glob.glob('./Input/*.tif')[0]
+    
 
     print('Getting the mask file...')
     shp_file_path = maskPath
     if shp_file_path =='.\Buffer\Buffer.shp':
         print('Creating the output file name...')
-        output_raster_path = src_raster_path
+        outputRasterPath = input_raster_path
     else:
         print('Creating the output file name...')
-        output_raster_path = './Output/' + os.path.basename(src_raster_path)
+        
 
     print('Opening the mask file... ')
     with fiona.open(shp_file_path, "r") as shapefile:
         shapes = [feature["geometry"] for feature in shapefile]
         
     print('Opening the image file...')
-    with rasterio.open(src_raster_path) as src:
+    with rasterio.open(input_raster_path) as src:
         print('Clipping the image...')
         out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
         out_meta = src.meta
@@ -38,7 +37,7 @@ def Clip(maskPath):
                     "transform": out_transform})
 
     print('Saving the output file...')
-    with rasterio.open(output_raster_path, "w", **out_meta) as dest:
+    with rasterio.open(outputRasterPath, "w", **out_meta) as dest:
         dest.write(out_image)
         
     print('Done')
